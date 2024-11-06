a51aa770152c3ab0ef1c22f9499676dae82fd7dc
Nojus Petrušis <petrusis.nojus@gmail.com>
2024-11-06 16:43:45 +0200

    sutvarkytas API access visiem users

--- FitQuest/obj/Debug/net8.0/FitQuest.AssemblyInfo.cs
+++ FitQuest/obj/Debug/net8.0/FitQuest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitQuest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57a4308fc7099b512627c964b34bb35faf888f66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a5846b011a74f62172455a5cb0cd15707457da3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitQuest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitQuest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
