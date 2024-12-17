b6af097953c738008df6eaa71ba9a138efb5f706
AnastasijaFed <nastefedorenko@gmail.com>
2024-12-17 09:29:34 +0200

    front profile

--- FitQuest/obj/Debug/net8.0/FitQuest.AssemblyInfo.cs
+++ FitQuest/obj/Debug/net8.0/FitQuest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitQuest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79f5fa09085748d8b0e355a7a22871c130968d43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3090228f48797309c35b04055d48fd4376f019")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitQuest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitQuest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
